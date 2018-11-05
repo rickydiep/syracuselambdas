4dc14be3ff0b5aca32d9c79c267fa36368d8e3ff
rickydiep <35702442+rickydiep@users.noreply.github.com>
2018-11-05 16:27:52 -0600

    minifed assets

--- file.js
+++ file.js
@@ -1,16 +1,1 @@
-$(document).ready(function(){  
-    var feed = new Instafeed ({
-        get:"user",
-        userId:"1531587193",
-        limit: 4,
-        sortBy: "most-recent",
-        resolution: "standard_resolution",
-        accessToken: "1531587193.1677ed0.a24077c78de34f6a9fcd7538c6fb6c98",
-        template: '<div class="image-box "><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); width="85px;"></div></a></div>'
-});
-    feed.run(); 
-
-    $('.nav-link').on('click', function(){
-        $('#navbarNav').removeClass('show');
-    });
-});
+$(document).ready(function(){new Instafeed({get:"user",userId:"1531587193",limit:4,sortBy:"most-recent",resolution:"standard_resolution",accessToken:"1531587193.1677ed0.a24077c78de34f6a9fcd7538c6fb6c98",template:'<div class="image-box "><a href="{{link}}" target="_blank"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); width="85px;"></div></a></div>'}).run(),$(".nav-link").on("click",function(){$("#navbarNav").removeClass("show")})});
